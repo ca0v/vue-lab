a681b40533a982f4c59163f3e23c0b7a8a99e47d
COREY ALIX <corey.alix@infor.com>
2021-10-22 22:30:09 -0400

    generalize grid size, make accessible

---
+++ createGameBoard.js
@@ -0,0 +1,14 @@
+import { range } from "./fun/range.js";
+
+export function createGameBoard(size = 3) {
+  const cells = range(size).map((_, colIndex) =>
+    range(size).map((_, rowIndex) => ({
+      name: `${1 + colIndex * size + rowIndex}`,
+      colIndex,
+      rowIndex,
+      value: "?",
+      focus: false,
+    }))
+  );
+  return cells;
+}
--- index.js
+++ index.js
@@ -1,69 +1,171 @@
+/**
+ * this is an older pattern
+ * the composition pattern, which uses startup(), might simplify
+ * and promote de-composition.
+ */
+import { createGameBoard } from "./createGameBoard.js";
 import { log } from "./fun/log.js";
 import { range } from "./fun/range.js";
+
+function toggle(cell) {
+  cell.value = cell.value === "X" ? "O" : "X";
+}
+
+function findActiveCell(state) {
+  const activeColumn = state.find((column) => column.find((row) => row.focus));
+  return activeColumn?.find((row) => row.focus);
+}
+
+const gridSize = 7;
 
 export function run() {
   const app = Vue.createApp({
     template: `<div>This is the app</div>
     <tic-tac-toe v-on:toggleCell=toggleCell v-bind:state=gameState winner="?">
     </tic-tac-toe>`,
+    startup() {
+      log("startup1");
+    },
     methods: {
+      startup() {
+        log("startup2");
+      },
       play(move) {
         let { row, col, piece } = move;
-        log(JSON.stringify(move)); // how to call play on the tic-tac-toe component?
-        this.gameState[col][row] = piece;
+        this.gameState[col][row].value = piece;
       },
       toggleCell({ row, col }) {
-        this.gameState[row][col] = this.gameState[row][col] === "X" ? "O" : "X";
+        toggle(this.gameState[row][col]);
       },
     },
     data() {
       return {
-        gameState: [
-          ["7", "8", "9"],
-          ["4", "5", "6"],
-          ["1", "2", "3"],
-        ],
+        gameState: createGameBoard(gridSize),
       };
     },
   });
 
   // I am having trouble expressing a N x N grid
   // perhaps I should use a css grid
-  const template = range(3)
-    .map((_) => range(3))
-    .map((row, rowIndex) =>
-      row
-        .map(
-          (col, colIndex) =>
-            `<ttt-cell v-on:toggled=toggleCell(${rowIndex},${colIndex}) v-bind:state=state[${rowIndex}][${colIndex}]></ttt-cell>`
-        )
-        .join("")
-    )
-    .map((rowTemplate) => `<div>${rowTemplate}</div>`)
-    .join("");
+  const createTemplate = (size = 3) =>
+    range(size)
+      .map((_) => range(size))
+      .map((col, x) =>
+        col
+          .map(
+            (cell, y) =>
+              `<ttt-cell ref="cell_${x}_${y}" v-on:toggled=toggleCell(${x},${y}) v-bind:state=state[${x}][${y}]></ttt-cell>`
+          )
+          .join("")
+      )
+      .map((rowTemplate) => `<div>${rowTemplate}</div>`)
+      .join("");
 
   app.component("tic-tac-toe", {
     template: `<p>{{winner}}</p>
-    <div class="tic-tac-toe">
-    ${template}
+    <div class="tic-tac-toe" @keypress="keypress">
+    ${createTemplate(gridSize)}
     </div>`,
-    props: ["winner", "state"],
+    props: ["winner", "state", "gridSize"],
     emits: ["toggleCell"],
     methods: {
       toggleCell(row, col) {
         this.$emit("toggleCell", { row, col });
         log("toggleCell", JSON.stringify(this.state));
       },
+      toggleActiveCell() {
+        const activeCell = findActiveCell(this.state);
+        activeCell && toggle(activeCell);
+      },
+      keypress(event) {
+        const refs = this.$refs;
+
+        switch (event.code) {
+          case "Space":
+          case "NumpadEnter":
+          case "Enter":
+            this.toggleActiveCell();
+            break;
+          case "Digit1":
+          case "Digit2":
+          case "Digit3":
+          case "Digit4":
+          case "Digit5":
+          case "Digit6":
+          case "Digit7":
+          case "Digit8":
+          case "Digit9":
+          case "Numpad1":
+          case "Numpad2":
+          case "Numpad3":
+          case "Numpad4":
+          case "Numpad5":
+          case "Numpad6":
+          case "Numpad7":
+          case "Numpad8":
+          case "Numpad9":
+            {
+              const cell = event.keyCode - 49;
+              const x = cell % gridSize;
+              const y = Math.floor(cell / gridSize);
+              this.$refs[`cell_${x}_${y}`].$el.focus();
+            }
+            break;
+          case "KeyA":
+          case "KeyD":
+          case "KeyW":
+          case "KeyS":
+            {
+              const activeCell = findActiveCell(this.state);
+              if (activeCell) {
+                let { colIndex, rowIndex } = activeCell;
+                switch (event.code) {
+                  case "KeyA":
+                    log("move left");
+                    rowIndex = (rowIndex + gridSize - 1) % gridSize;
+                    break;
+                  case "KeyD":
+                    log("move right");
+                    rowIndex = (rowIndex + gridSize + 1) % gridSize;
+                    break;
+                  case "KeyW":
+                    log("move up");
+                    colIndex = (colIndex + gridSize - 1) % gridSize;
+                    break;
+                  case "KeyS":
+                    log("move down");
+                    colIndex = (colIndex + gridSize + 1) % gridSize;
+                    break;
+                }
+                focusCell(colIndex, rowIndex);
+              }
+            }
+            break;
+          default:
+            log("grid", event.code);
+            break;
+        }
+
+        function focusCell(colIndex, rowIndex) {
+          refs[`cell_${colIndex}_${rowIndex}`].$el.focus();
+        }
+      },
     },
   });
 
   app.component("ttt-cell", {
-    template: `<span class="ttt-cell" @click="toggle()">{{state}}</span>`,
+    template: `<span tabIndex="0" class="ttt-cell" @focus="focus" @focusout="unfocus" @click="toggle">{{state.value}}</span>`,
     props: ["state"],
     methods: {
       toggle() {
         log("ttt-cell:toggle");
         this.$emit("toggled");
+      },
+      focus() {
+        this.state.focus = true;
+      },
+      unfocus() {
+        this.state.focus = false;
       },
     },
   });
