38c1a5d675c7bf28ea42e4bbffb19586a6412c40
Corey Alix <coreyalix@gmail.com>
2023-01-06 19:01:35 -0500

    Utilize the "name" field

--- vue-dotnet/vue-for-webapi/src/components/Main.vue
+++ vue-dotnet/vue-for-webapi/src/components/Main.vue
@@ -1,25 +1,23 @@
 <script setup lang="ts">
 import { reactive } from "vue";
 import {api1 as api} from "../services/fetch"
+import type {components} from "../ApiProxy"
 
 async function getData() {
   const data = await api.getMyDatabase();
-  if (!data.length) {
-    data.push({id: -5}, {id: -7})
-  }
   return data;
 }
 
 // set data into an observable model
-const startupData = [{id: 1}, {id: 2}]
+const startupData: Array<components["schemas"]["MyTable"]> = []
 const reactiveData = reactive({startupData})
 const data = reactiveData.startupData
 
-async function loadData() {
+async function refreshData() {
   const data = await getData();
-  console.log("loadData", {data})
-  // eliminate any blank ids
-  const dataWithKeys = data.filter(item => item.id).map(v => ({...v, id: v.id!}))
+  // the proxy gen is not perfect, so we need to cast the data
+ const dataWithKeys = data as components["schemas"]["MyTable"][] & {id: number};
+ reactiveData.startupData.length = 0;
   reactiveData.startupData.push(...dataWithKeys)
 }
 </script>
@@ -30,10 +28,10 @@ async function loadData() {
     <h2>No data</h2>
     <p>There is no data to show.</p>
   </div>
-  <button v-on:click="() => loadData()">Load Data</button>
+  <button v-on:click="() => refreshData()">Load Data</button>
   <!-- show each data item in a grid -->
   <div v-for="item in data" :key="item.id">
     <h2>{{item.id}}</h2>
-    <p>There is no description for item {{item.id}} just yet.</p>
+    <p>{{item.name}}</p>
   </div>
 </template>
